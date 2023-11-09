4833de2a16b9446345cbb8ccb235c5f6bf0000fc
TornaxO7 <tornax@proton.me>
2023-11-09 15:16:46 +0100

    adding flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      legacyPackages.${system}."wgsl-analyzer" = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "wgsl-analyzer";
+        version = "0.8.0";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "wgsl-analyzer";
+          repo = pname;
+          rev = version;
+          sha256 = "";
+        };
+
+        cargoSha256 = "";
+
+        meta = with pkgs.stdenv.lib; {
+          description = "A language server implementation for the WGSL shading language";
+          homepage = "https://github.com/wgsl-analyzer/wgsl-analyzer";
+          license = licenses.mit;
+          maintainers = [ maintainers.tailhook ];
+        };
+      };
+    };
+}
