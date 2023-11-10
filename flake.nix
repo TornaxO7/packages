1307a242d78fea38c7bb9724faa3857ff33178e9
TornaxO7 <tornax@proton.me>
2023-11-10 18:53:04 +0100

    adding package overlay

--- flake.nix
+++ flake.nix
@@ -8,27 +8,37 @@
       system = "x86_64-linux";
 
       pkgs = import nixpkgs { inherit system; };
+
+      mkWgslAnalyzer =
+        { lib
+        , ...
+        }:
+        pkgs.rustPlatform.buildRustPackage rec {
+          pname = "wgsl-analyzer";
+          version = "0.8.0";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "wgsl-analyzer";
+            repo = pname;
+            rev = version;
+            sha256 = "";
+          };
+
+          cargoSha256 = "";
+
+          meta = with pkgs.stdenv.lib; {
+            description = "A language server implementation for the WGSL shading language";
+            homepage = "https://github.com/wgsl-analyzer/wgsl-analyzer";
+            license = licenses.mit;
+            maintainers = [ maintainers.tailhook ];
+          };
+        };
     in
     {
-      legacyPackages.${system}."wgsl-analyzer" = pkgs.rustPlatform.buildRustPackage rec {
-        pname = "wgsl-analyzer";
-        version = "0.8.0";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "wgsl-analyzer";
-          repo = pname;
-          rev = version;
-          sha256 = "";
-        };
-
-        cargoSha256 = "";
-
-        meta = with pkgs.stdenv.lib; {
-          description = "A language server implementation for the WGSL shading language";
-          homepage = "https://github.com/wgsl-analyzer/wgsl-analyzer";
-          license = licenses.mit;
-          maintainers = [ maintainers.tailhook ];
-        };
+      overlays.wsgl-analyzer = final: prev: {
+        wgsl-analyzer = pkgs.callPackage mkWgslAnalyzer { };
       };
+
     };
 }
